9404db993ed460cee517a62984caf92478878e92
Perkins, Thomas <thomas.perkins@novartis.com>
2020-06-26 15:31:48 -0400

    updated title

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-            <Link to="/"> Home</Link>
+            <Link to="/"> Fib Calculator</Link>
             <Link to="/otherpage">Other Page</Link>
           <a
             className="App-link"
